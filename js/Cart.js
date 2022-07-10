eb73b9e7c461b62fcdaa51b0f3a9230a664b8a61
Taki123s <20130305@st.hcmuaf.edu.vn>
2022-07-11 00:40:59 +0700

    no commit

---
+++ js/Cart.js
@@ -0,0 +1,290 @@
+var navres = document.getElementById("more-nav");
+var boxmore = document.getElementById("box-more");
+var getlistproduct = localStorage.getItem("item");
+var getcart = localStorage.getItem("cart");
+var valueGetCart = JSON.parse(getcart);
+var valueGetProduct = JSON.parse(getlistproduct);
+var listCart;
+navres.onclick = function () {
+  boxmore.classList.toggle("visible");
+};
+function delay(){
+  document.body.classList.remove('hidden')
+  document.getElementById('login-ctn').style.animation='rotation 0.6s infinite linear';
+  document.getElementById('login-ctn').style.animationIterationCount='1';
+}
+function addItemtoCart(name, value, img, price, priceforone, status, username) {
+  //render sản phẩm khi đã có đủ dữ liệu
+  var cartItem = {
+    name: name,
+    value: value,
+    img: img,
+    price: price,
+    priceforone: priceforone,
+    status: status,
+    username: username,
+  };
+  var check = document
+    .querySelector(".js-HandlerLR")
+    .classList.contains("js-isLogin");
+  if (!check || username == "no login"){
+
+  document.getElementById("dangnhap").style.visibility = "visible";
+  setTimeout(delay,0)
+  }
+  else {
+    for (var i = 0; i < valueGetCart.length; i++) {
+      if (valueGetCart[i].name == name) {
+        if (valueGetCart[i].username == username) {
+          var h = parseInt(valueGetCart[i].value);
+          var k = parseInt(valueGetCart[i].price);
+          h += parseInt(value);
+          k = parseInt(priceforone) * h;
+          valueGetCart.splice(i, 1);
+          cartItem.value = h;
+          cartItem.price = k;
+        }
+      }
+    }
+    valueGetCart.push(cartItem);
+    localStorage.setItem("cart", JSON.stringify(valueGetCart));
+    deleteItemfromCart();
+    additem();
+    subtractitem();
+    renderCart();
+    swal.fire({
+      icon: 'success',
+      title: 'Đã thêm vào giỏ hàng ',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+}
+
+function setObjectCart() {
+  // tạo dữ liệu cho sản phẩm rồi push lên local storage + render
+  var getindex = document.getElementsByClassName("them");
+  var getnamebyselect = document.getElementsByClassName("ten");
+  var getusername = document.getElementById("js-Username");
+  var valueGetProduct = JSON.parse(localStorage.getItem('item'));
+  var valueGetCart = JSON.parse(localStorage.getItem('cart'));
+  if (getusername == null) {
+    getusername = "no login";
+  }
+  for (let j = 0; j < getindex.length; j++) {
+    getindex[j].onclick = function () {
+      var findvalue = $(getindex[j]).closest("div").find("input");
+      var getname = getnamebyselect[j].textContent;
+      for (let i = 0; i < valueGetProduct.length; i++) {
+        if (getname == valueGetProduct[i].name) {
+          addItemtoCart(
+            getname,
+            findvalue.val(),
+            valueGetProduct[i].src,
+            parseInt(valueGetProduct[i].price) * findvalue.val(),
+            parseInt(valueGetProduct[i].price),
+            "unconfimred",
+            getusername.innerText
+          );
+        }
+      }
+    };
+  }
+}
+function gotoCart() {
+  //chuyển main content sang cart
+  var m = document.querySelectorAll(".header-cart");
+  var l = document.getElementById("content-left");
+  var r = document.getElementById("content-right");
+  var ctn = document.getElementById("container-cart");
+  var ctc = document.getElementById("content-cart");
+  var getusername = document.getElementById("js-Username");
+
+    for(let element of m){
+      element.onclick = function () {
+        var x = document.getElementsByClassName("sas");
+        if(getusername==null){
+          document.getElementById("dangnhap").style.visibility = "visible";
+          setTimeout(delay,0)
+        }
+        else{
+          for (let j = 0; j < x.length; j++) {
+            x[j].classList.remove("nav-active");
+          }
+        l.style.display = "none";
+        r.style.display = "none";
+        ctn.style.display = "block";
+        ctc.style.display = "block";
+        setObjectCart();
+        renderCart();
+        deleteItemfromCart();
+        additem();
+        subtractitem();
+        renderWaitCart();
+        deleteItemWaiCart();
+        }
+      };
+    }
+  
+ 
+}
+
+function renderCart() {
+  //hàm render cart
+
+  var result = `<tr>
+   <th>Sản Phẩm</th>
+   <th>Đơn Giá</th>
+   <th>Số Lượng</th>
+   <th>Thành Tiền</th>
+   <th>Thao Tác</th>
+</tr>`;
+  var k = 0;
+  var sumPrice = 0;
+  for (let i = 0; i < valueGetCart.length; i++) {
+    var getusername = document.getElementById("js-Username");
+    if (getusername == null) {
+      getusername = "no login";
+    }
+    if (
+      valueGetCart[i].username == getusername.innerText &&
+      valueGetCart[i].status == "unconfimred"
+    ) {
+      k++;
+      result += `<tr>
+      <td><img src=${valueGetCart[i].img}>
+         <p class="nameOfCart">${valueGetCart[i].name}</p>
+      </td><td>
+      ${valueGetCart[i].priceforone} đồng
+      </td>
+      <td><button class="subtractCart" id="subtractCart" >-</button> <p class="valueCart">${valueGetCart[i].value}</p> <button class="addCart" id="addCart">+</button></td>
+      <td>${valueGetCart[i].price} đồng</td>
+      <td><Button id="deleteItem"  class="deleteItem">Xóa</Button></td>
+   </tr>`;
+      sumPrice += parseInt(valueGetCart[i].price);
+    }
+  }
+  result += `<td><p>Tổng tiền :${sumPrice} đồng</p></td>`;
+  result += `<td></td>`;
+  result += `<td></td>`;
+  result += `<td></td>`;
+  result += `<td><Button id="order" onclick="setUp()">Đặt Hàng</Button></td>`;
+  if (k == 0) {
+    result = `<img src="Image/cart.png" alt="" srcset="">`;
+
+    setObjectCart();
+    deleteItemfromCart();
+    additem();
+    subtractitem();
+  } else {
+    setObjectCart();
+    deleteItemfromCart();
+    additem();
+    subtractitem();
+  }
+
+  document.getElementById("tableCart").innerHTML = result;
+}
+
+function deleteItemfromCart() {
+  //xóa sản phẩm khỏi cart
+  var m = document.getElementsByClassName("deleteItem");
+  for (let i = 0; i < m.length; i++) {
+    m[i].onclick = function () {
+      Swal.fire({
+        title: 'Thông báo!',
+        text: "Bạn có chắc chắn muốn xóa món hàng này?",
+        icon: 'warning',
+        showCancelButton: true,
+        cancelButtonText:'Không',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Đồng ý',
+      
+      }).then((result) => {
+        if (result.isConfirmed) {
+          valueGetCart.splice(i, 1);
+          localStorage.setItem("cart", JSON.stringify(valueGetCart));
+          setObjectCart();
+          renderCart();
+          deleteItemfromCart();
+          additem();
+          subtractitem();
+          Swal.fire(
+            'Deleted!',
+            'Món hàng đã được xóa',
+            'success'
+          )
+        }
+      })
+    };
+  }
+}
+function additem() {
+  //thêm số lượng sản phẩm cùng loại
+  var getindex = document.getElementsByClassName("addCart");
+  var getIndexP = document.getElementsByClassName("valueCart");
+  for (let j = 0; j < getindex.length; j++) {
+    getindex[j].onclick = function () {
+      var getusername = document.getElementById("js-Username");
+      var findParent = $(getindex[j]).closest("tr").find("p");
+      for (var i = 0; i < valueGetCart.length; i++) {
+        if (valueGetCart[i].name == findParent[0].innerText) {
+          if (valueGetCart[i].username == getusername.innerText) {
+            var valuee = valueGetCart[i].value;
+            valueGetCart[i].value = parseInt(valuee) + 1;
+            valueGetCart[i].price =
+              parseInt(valueGetCart[i].value) *
+              parseInt(valueGetCart[i].priceforone);
+          }
+        }
+      }
+      localStorage.setItem("cart", JSON.stringify(valueGetCart));
+      setObjectCart();
+      renderCart();
+      deleteItemfromCart();
+      additem();
+      subtractitem();
+    };
+  }
+}
+function subtractitem() {
+  //giảm số lượng sản phẩm cùng loại
+  var getindex = document.getElementsByClassName("subtractCart");
+  for (let j = 0; j < getindex.length; j++) {
+    getindex[j].onclick = function () {
+      var m = valueGetCart[j].value;
+      if (m >= 2) {
+        valueGetCart[j].value = parseInt(m) - 1;
+        valueGetCart[j].price =
+          parseInt(valueGetCart[j].priceforone) *
+          parseInt(valueGetCart[j].value);
+        localStorage.setItem("cart", JSON.stringify(valueGetCart));
+        setObjectCart();
+        renderCart();
+        deleteItemfromCart();
+        additem();
+        subtractitem();
+      } else {
+        valueGetCart[j].value = parseInt(1);
+        valueGetCart[j].price =
+          parseInt(valueGetCart[j].priceforone) *
+          parseInt(valueGetCart[j].value);
+        localStorage.setItem("cart", JSON.stringify(valueGetCart));
+        setObjectCart();
+        renderCart();
+        deleteItemfromCart();
+        additem();
+        subtractitem();
+      }
+    };
+  }
+}
+
+createCart();
+gotoCart();
+setObjectCart();
+renderCart();
+deleteItemfromCart();
+additem();
+subtractitem();
---
+++ js/waitingCart.js
@@ -0,0 +1,200 @@
+let getWaitButton = document.getElementById("order");
+var listwaitingitem;
+
+function getwaitingcart() {
+    var getlistwaitingcart = localStorage.getItem("waitItem");
+    if (getlistwaitingcart) {
+        listwaitingitem = JSON.parse(getlistwaitingcart);
+    } else {
+        listwaitingitem = [];
+    }
+    localStorage.setItem("waitItem", JSON.stringify(listwaitingitem));
+}
+
+function pushItem() {
+    var getusername = document.getElementById("js-Username");
+    var tinh = $('select[name="calc_shipping_provinces"]')
+    var listwaitingitem = JSON.parse(localStorage.getItem("waitItem"));
+    var quan = $('select[name="calc_shipping_district"]');
+    var sdt = document.getElementById('phone').value;
+    var arrayClone = valueGetCart.filter(function(item, index) {
+        return item.username === getusername.innerText;
+    });
+    var sumTotal = arrayClone.reduce((a, b) => a + b.price, 0);
+    var statusArray = arrayClone.map(function(item) {
+        return {
+            img: item.img,
+            name: item.name,
+            price: item.price,
+            priceforone: item.priceforone,
+            status: "Waiting",
+            username: item.username,
+            value: item.value,
+        };
+    });
+
+    listwaitingitem.push({
+        username: getusername.innerText,
+        cart: statusArray,
+        sumTotal: sumTotal,
+        status: "Waiting",
+        address: "Tỉnh " + tinh.children('option:selected').text() + "-" + quan.children('option:selected').text(),
+        phone: sdt,
+    });
+
+    localStorage.setItem("waitItem", JSON.stringify(listwaitingitem));
+    var arrayCartNew = valueGetCart.filter(function(item, index) {
+        return item.username !== getusername.innerText;
+    });
+    localStorage.setItem("cart", JSON.stringify(arrayCartNew));
+    valueGetCart = arrayCartNew;
+    setObjectCart();
+    renderCart();
+    deleteItemfromCart();
+    additem();
+    subtractitem();
+    renderWaitCart();
+    deleteItemWaiCart()
+}
+
+function renderWaitCart() {
+    var getusername;
+    if (document.getElementById("js-Username")) {
+        getusername = document.getElementById("js-Username")
+    } else {
+        getusername = 'no login'
+    }
+    var listwaitingitem = JSON.parse(localStorage.getItem("waitItem"));
+    var result = `<tr>
+  <th>Stt</th>
+  <th>Sản phẩm</th>
+  <th>Tổng tiền</th>
+  <th>Địa chỉ</th>
+  <th>Số điện thoại</th>
+  <th>Trạng thái</th>
+  <th>Thao tác</th> 
+</tr>`;
+    var findItem = listwaitingitem.filter(function(item) {
+        return item.username === getusername.innerText;
+    });
+    var renderItem = findItem.map(function(item, index) {
+        var k = item.cart.map((a) => {
+            return a.name + ` x ` + a.value + `<br>`;
+        });
+        var color = 'green'
+        var statusInner='Đã xác nhận'
+        if (item.status == 'Deny') {
+            color = 'red'
+            statusInner='Đã bị từ chối'
+        } else if (item.status == 'Waiting') {
+            color = 'blue'
+            statusInner='Đang chờ'
+        }
+        return `<tr> <td>${
+      index + 1
+    }</td> 
+    <td><p>${k}</p></td> 
+    <td>${item.sumTotal} đồng</td>
+    <td>${item.address}</td>
+    <td>${item.phone}</td> 
+    <td style="color:${color}!important;">${statusInner}</td> 
+    <td><a href="javascript:;" class="fa-solid fa-rectangle-xmark deleteWaitCart"></a></td>
+    </tr>`;
+    });
+
+    result += renderItem.toString().replace(/,/g, " ");
+
+    document.getElementById("waitCart").innerHTML = result;
+}
+
+function setUp() {
+    Swal.fire({
+        title: 'Thông báo!',
+        text: "Bạn muốn xác nhận mua đơn hàng này ?",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonText:'Không',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Đồng ý'
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+            $("#dialog-1").dialog("open");
+          
+
+        }
+    })
+}
+
+function selectItem() {
+    var tinh = $('select[name="calc_shipping_provinces"]')
+    var quan = $('select[name="calc_shipping_district"]');
+    var sdt = document.getElementById('phone').value;
+
+
+    if (tinh.children('option:selected').text() != 'Tỉnh / Thành phố' && quan.children('option:selected').text() != 'Quận / Huyện' && !isNaN(sdt) &&sdt.length>=10) {
+        pushItem()
+        $("#dialog-1").dialog("close");
+        Swal.fire(
+            'Thành công!',
+            'Đơn hàng của bạn đã được lưu, vui lòng chờ người quản lí xác nhận',
+            'success'
+        )
+    } else {
+        $("#dialog-1").dialog("close");
+        Swal.fire(
+            'Không thành công!',
+            'Dữ liệu không hợp lệ',
+            'warning'
+        )
+    }
+
+}
+function deleteItemWaiCart(){
+    let getindex = document.getElementsByClassName("deleteWaitCart")
+    var listwaitingitem = JSON.parse(localStorage.getItem("waitItem"));
+    var getusername;
+    if (document.getElementById("js-Username")) {
+        getusername = document.getElementById("js-Username")
+    } else {
+        getusername = 'no login'
+    }
+    var listwaitingitem = JSON.parse(localStorage.getItem("waitItem"));
+    var indexWaitCart= listwaitingitem.map((a,b)=>{
+        return {item:a,index:b}
+    })
+    var findItem = indexWaitCart.filter(function(e) {
+        return e.item.username === getusername.innerText;
+    });
+    for(let i=0;i<getindex.length;i++){
+        getindex[i].onclick = function(){
+            Swal.fire({
+                title: 'Bạn chắc chắn muốn hủy đơn hàng này?',
+                icon:'warning',
+                showCancelButton: true,
+                cancelButtonText:'Không',
+                confirmButtonText: 'Có',
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                
+              }).then((result) => {
+           
+                if (result.isConfirmed) {
+                   if(findItem[i].item.status!='Accept'){
+                    listwaitingitem.splice(parseInt(findItem[i].index),1)
+                    localStorage.setItem("waitItem", JSON.stringify(listwaitingitem));
+                    renderWaitCart()
+                    deleteItemWaiCart()
+                  Swal.fire('Đơn hàng đã được hủy!', '', 'success')
+                   }else{
+                    Swal.fire('Đơn hàng đã được hệ thống xác nhận, không thể hủy!', '', 'warning')
+                   }
+                }
+              })
+        }
+    }
+}
+getwaitingcart();
+renderWaitCart();
+deleteItemWaiCart()
